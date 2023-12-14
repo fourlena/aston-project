cfad5b2e705c1bf06cd243ff5f92c3e5fa0a0828
fourlena <elavlovic@gmail.com>
2023-12-14 12:26:41 +0300

    add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes,Route} from "react-router-dom";
-import Main from "./pages/main/main";
+import Main from './pages/main/main';
 
 function App() {
-  return (
-      <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-            <Routes>
-                <Route path={'/'} element={<Main/>}/>
-            </Routes>
-        </div>
-      </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+				<Routes>
+					<Route path={'/'} element={<Main />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({});
 
 export const store = configureStore({
-    reducer: rootReducer,
-})
-// определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+	reducer: rootReducer
+});
+
+export type AppRootStateType = ReturnType<typeof rootReducer>;
 
 // @ts-ignore
-window.store = store
+window.store = store;
